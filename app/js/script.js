7808ef1e4d440b99923b09f25a0bb394d88f8f6c
Luis Fernando <luisfernandoavila92@gmail.com>
2021-04-21 18:47:44 -0300

    feat(header): create of hamburguer menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,9 @@
-console.log("hello workd");
+const hamburguerMenu = document.getElementById('hamburguer');
+
+hamburguerMenu?.addEventListener('click', function () {
+  const hasOpenClass = hamburguerMenu.classList.contains('open');
+
+  hasOpenClass
+    ? hamburguerMenu.classList.remove('open')
+    : hamburguerMenu.classList.add('open');
+});
