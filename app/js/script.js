215aacc78644eed1722fe1518e6ae3702d8c0e47
Luis Fernando <luisfernandoavila92@gmail.com>
2021-04-24 18:10:44 -0300

    feat: create overlay menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,16 @@
 const hamburguerMenu = document.getElementById('hamburguer');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 hamburguerMenu?.addEventListener('click', function () {
-  const hasOpenClass = hamburguerMenu.classList.contains('open');
-
-  hasOpenClass
-    ? hamburguerMenu.classList.remove('open')
-    : hamburguerMenu.classList.add('open');
+  const hasOpenClass = header.classList.contains('open');
+  if (hasOpenClass) {
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+    return;
+  }
+  header.classList.add('open');
+  overlay.classList.add('fade-in');
+  overlay.classList.remove('fade-out');
 });
