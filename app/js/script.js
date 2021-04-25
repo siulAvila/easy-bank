d7a91fcdaa274092006f6c96b6b391a3cf5ae54f
Luis Fernando <luisfernandoavila92@gmail.com>
2021-04-25 17:00:22 -0300

    feat: create menu toggle

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,30 @@
 const hamburguerMenu = document.getElementById('hamburguer');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const hasFadeElements = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
 
 hamburguerMenu?.addEventListener('click', function () {
   const hasOpenClass = header.classList.contains('open');
-  if (hasOpenClass) {
-    header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
-    return;
-  }
-  header.classList.add('open');
-  overlay.classList.add('fade-in');
-  overlay.classList.remove('fade-out');
+  hasOpenClass ? openHambuerguerMenu() : closeHamburguerMenu();
 });
+
+function openHambuerguerMenu() {
+  body.classList.remove('hidden-scroll');
+  header.classList.remove('open');
+
+  for (const element of hasFadeElements) {
+    element.classList.remove('fade-in');
+    element.classList.add('fade-out');
+  }
+}
+
+function closeHamburguerMenu() {
+  header.classList.add('open');
+  body.classList.add('hidden-scroll');
+
+  for (const element of hasFadeElements) {
+    element.classList.add('fade-in');
+    element.classList.remove('fade-out');
+  }
+}
